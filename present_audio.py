80c7189b722fcb54949c1c465a7fe77a07d9998d
R. Steiner <r.steiner@uconn.edu>
2017-02-20 16:13:15 -0500

    Cleanup some comments.

--- present_audio.py
+++ present_audio.py
@@ -17,21 +17,12 @@ def load_stims(filenames, shuffle=False):
 
 
 def audio_trial(waveobject, **kwargs):
-    jitter = kwargs.get("jitter", None)
-    jitter_mean = jitter["mean"] if jitter else None
-    jitter_sd = jitter["sd"] if jitter else None
-    iti = rd.gauss(jitter_mean, jitter_sd)/1000.0 if jitter\
-        else kwargs.get("iti")/1000.0
-    # stim = waveobject.play()
-    # stim.wait_done()
     waveobject.play().wait_done()
     print("next")
-    time.sleep(iti)
 
 prefix = "/home/rachael/Documents/School/kiloperf/stimuli/"\
     + "wordsbyword/words{}.wav"
 stims = load_stims([prefix.format("ache"), prefix.format("beast"),
                     prefix.format("ail")])
-# time.sleep(3)
 print("loaded stimuli")
 [audio_trial(stim, iti=1000) for stim in stims]
