f85ff9eca24bcf406e89bac45763cd89ccb5c6e7
R. Steiner <r.steiner@uconn.edu>
2017-02-20 16:46:15 -0500

    Handle button press & RT.

--- present_text.py
+++ present_text.py
@@ -8,27 +8,37 @@ what's going on.
 
 import time
 from sys import stdout
+import ncurses as nc
 
 
-def present_stimulus(stimulus, duration):
+def present_stimulus(stimulus, duration=3600000, window_obj, **kwargs):
+    allowed_keys = kwargs.get("allowed_keys", [])
     duration /= 1000.0
     stdout.write("\r{}".format(stimulus))
     stdout.flush()
-    time.sleep(duration)
+    response = ""
+    starttime = endtime = time.clock()
+    while endtime < starttime + duration:
+        key_pressed = window_obj.getkey()
+        endtime = time.clock()
+        if key_pressed in allowed_keys:
+            response = key_pressed
+            break
+    rt = endtime - starttime
     # Overwrite the stimulus:
     stdout.write("\r"+" "*len(stimulus))
     stdout.flush()
 
 
-def experiment(stimuli, trial_dur, iti):
-    iti /= 1000.0
-    # overwrite = " "*max([len(stim) for stim in stimuli])
-    for stim in stimuli:
-        present_stimulus(stim, trial_dur)
-        # stdout.write("\r{}".format(overwrite))
-        # stdout.flush()
-        time.sleep(iti)
-    stdout.write("\n")
-
-stims = ["hello", "world", "colorless", "green", "ideas", "sleep", "furiously"]
-experiment(stims, 500, 250)
+# def experiment(stimuli, trial_dur, iti):
+#     iti /= 1000.0
+#     # overwrite = " "*max([len(stim) for stim in stimuli])
+#     for stim in stimuli:
+#         present_stimulus(stim, trial_dur)
+#         # stdout.write("\r{}".format(overwrite))
+#         # stdout.flush()
+#         time.sleep(iti)
+#     stdout.write("\n")
+#
+# stims = ["hello", "world", "colorless", "green", "ideas", "sleep", "furiously"]
+# experiment(stims, 500, 250)
