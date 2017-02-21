c1c041b05eba87b1900325e51d83f820a7bca2dd
R. Steiner <r.steiner@uconn.edu>
2017-02-21 17:47:13 -0500

    Add audio trial.

--- psyexp/audio/audio.py
+++ psyexp/audio/audio.py
@@ -4,23 +4,33 @@
 Present auditory stimuli. WIP.
 """
 
-import simpleaudio as sa
-import time
-import random as rd
+# import simpleaudio as sa
+# import pygame as pyg
+from psyexp.experiment.trial import Trial
+# import time
+# import random as rd
 
 
-def load_audio_stims(filenames, shuffle=False):
-    rd.shuffle(filenames) if shuffle else None
-    return [sa.WaveObject.from_wave_file(fp) for fp in filenames]
+class AudioTrial(Trial):
+    def __init__(self, stimulus, **kwargs):
+        super().__init__(**kwargs)
+        # Assume stimulus is a pre-loaded pyg.media object
+        self.stimulus = stimulus
+        self.stimulus.play()
 
 
-def audio_trial(waveobject, **kwargs):
-    waveobject.play().wait_done()
-    print("next")
-
-prefix = "/home/rachael/Documents/School/kiloperf/stimuli/"\
-    + "wordsbyword/words{}.wav"
-stims = load_stims([prefix.format("ache"), prefix.format("beast"),
-                    prefix.format("ail")])
-print("loaded stimuli")
-[audio_trial(stim, iti=1000) for stim in stims]
+# def load_audio_stims(filenames, shuffle=False):
+#     rd.shuffle(filenames) if shuffle else None
+#     return [sa.WaveObject.from_wave_file(fp) for fp in filenames]
+#
+#
+# def audio_trial(waveobject, **kwargs):
+#     waveobject.play().wait_done()
+#     print("next")
+#
+# prefix = "/home/rachael/Documents/School/kiloperf/stimuli/"\
+#     + "wordsbyword/words{}.wav"
+# stims = load_stims([prefix.format("ache"), prefix.format("beast"),
+#                     prefix.format("ail")])
+# print("loaded stimuli")
+# [audio_trial(stim, iti=1000) for stim in stims]
