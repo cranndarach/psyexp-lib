3abc98e430587a38b534e402ae1293c0641f9fc7
R. Steiner <r.steiner@uconn.edu>
2017-02-21 17:51:06 -0500

    Start writing Task class (come back here).

--- psyexp/experiment/experiment.py
+++ psyexp/experiment/experiment.py
@@ -11,27 +11,37 @@ import random as rd
 import pyglet as pyg
 
 
-def task(trials, **kwargs):
-    jitter = kwargs.get("jitter", None)
-    jitter_mean = jitter["mean"] if jitter else None
-    jitter_sd = jitter["sd"] if jitter else None
-    iti = rd.gauss(jitter_mean, jitter_sd)/1000.0 if jitter\
-        else kwargs.get("iti")/1000.0
-    # win = nc.initscr()
-    # win.nodelay(1)
-    [[trial, time.sleep(iti)] for trial in trials]
-    # stdout.write("\n")
-    # In case it becomes important to think about synchrony:
-    return True
+class Task:
+    def __init__(self, trials, **kwargs):
+        self.jitter = kwargs.get("jitter", None)
+        self.jitter_mean = self.jitter["mean"] if self.jitter else None
+        self.jitter_sd = self.jitter["sd"] if self.jitter else None
+        self.iti = rd.gauss(self.jitter_mean, self.jitter_sd)/1000.0 if\
+            self.jitter else kwargs.get("iti")/1000.0
 
+    # Not done!!
 
-def experiment(sequence):
-    win = pyg.window.Window()
-
-    @win.event
-    def on_draw():
-        win.clear()
-        [task for task in sequence]
-
-    # return?
-    pyg.app.run()
+# def task(trials, **kwargs):
+#     jitter = kwargs.get("jitter", None)
+#     jitter_mean = jitter["mean"] if jitter else None
+#     jitter_sd = jitter["sd"] if jitter else None
+#     iti = rd.gauss(jitter_mean, jitter_sd)/1000.0 if jitter\
+#         else kwargs.get("iti")/1000.0
+#     # win = nc.initscr()
+#     # win.nodelay(1)
+#     [[trial, time.sleep(iti)] for trial in trials]
+#     # stdout.write("\n")
+#     # In case it becomes important to think about synchrony:
+#     return True
+#
+#
+# def experiment(sequence):
+#     win = pyg.window.Window()
+#
+#     @win.event
+#     def on_draw():
+#         win.clear()
+#         [task for task in sequence]
+#
+#     # return?
+#     pyg.app.run()
