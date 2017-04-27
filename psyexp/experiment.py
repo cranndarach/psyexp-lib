f37e6b7322ffd816fa7eb3b69e701dd3b2c3647b
R Steiner <r.steiner@uconn.edu>
2017-04-27 15:06:27 -0400

    Shift argument structure of run_task, not quite ready.

--- psyexp/experiment.py
+++ psyexp/experiment.py
@@ -12,6 +12,33 @@ import csv
 # import numpy as np
 
 
+# class Experiment(pyg.app.EventLoop):
+#     def __init__(self, **kwargs):
+#         super().__init__()
+#         # fill in formatting stuff later
+#         self.win = pyg.window.Window()
+#         self.allowed_keys = []
+#
+#     def on_draw(self):
+#         self.win.clear()
+#         self.draw()
+#
+#     def on_key_press(self, symbol, modifiers):
+#         key = pyg.key.symbol_string(symbol)
+#         if key in self.allowed_keys:
+#             self.endtime = time.clock()
+#             self.response = key
+#             self.rt = self.endtime - self.starttime
+#             self.win.clear()
+
+
+# Okay so instead of a class per trial type, instead there should be a
+# trial runner, and the parameters and everything can be passed around.
+# Then we can skip most steps and just pass the stim list to this.
+def generate_task(stimlist, trial_runner, **kwargs):
+    gen = (trial_runner(stimulus=stim, **kwargs) for stim in stimlist)
+    return gen, kwargs
+
 class Experiment(pyg.app.EventLoop):
     def __init__(self, **kwargs):
         super().__init__()
@@ -32,31 +59,18 @@ class Experiment(pyg.app.EventLoop):
             self.win.clear()
 
 
-# Okay so instead of a class per trial type, instead there should be a
-# trial runner, and the parameters and everything can be passed around.
-# Then we can skip most steps and just pass the stim list to this.
-def generate_task(stimlist, trial_runner, **kwargs):
-    gen = (trial_runner(stimulus=stim, **kwargs) for stim in stimlist)
-    return gen, kwargs
-
-
-def run_task(*args, **kwargs):
-    # If they pass a template, use it, and if they pass
-    # a sequence of named arguments instead, use them:
-    exp = kwargs["experiment"]
-    stimuli = kwargs["stimuli"]
-    template = kwargs.get("template", kwargs)
-    # If they pass the generator as the first argument, use it;
-    # if they pass it as a named argument or as part of the
-    # template, use that:
-    gen = args[0] if args else template["gen"]
-    jitter = template.get("jitter", False)
+# The generate_task function returns a pair containing (generator, kwargs).
+def run_task(*args):
+    gen = args[0]
+    **kwargs = args[1]
+    jitter = kwargs.get("jitter", False)
     if jitter:
-        jitter_mean = template["jitter_mean"]
-        jitter_sd = template["jitter_sd"]
+        jitter_mean = kwargs["jitter_mean"]
+        jitter_sd = kwargs["jitter_sd"]
     else:
-        iti = template.get("iti", 0)/1000.0
+        iti = kwargs.get("iti", 0)/1000.0
         print("Warning: no ITI given. Defaulting to 0.") if not iti else None
+    additional_fields = kwargs.get("additional_fields", {})
     data = []
     for stim in stimuli:
         # I don't think this try...except format is needed, but I'm
@@ -68,7 +82,7 @@ def run_task(*args, **kwargs):
                         "EndTime": exp.endtime,
                         "RT": exp.rt,
                         "Response": exp.response}
-        trial_data = {**stimulus, **trial_results}
+        trial_data = {**stimulus, **trial_results, **additional_fields}
         data.append(trial_data)
         if jitter:
             time.sleep(rd.gauss(jitter_mean, jitter_sd))
