799cd2832f62bb07e636f89d785a7fce811f6eda
R Steiner <r.steiner@uconn.edu>
2017-04-27 14:47:46 -0400

    Create text trial runner and update test accordingly.

--- psyexp/text.py
+++ psyexp/text.py
@@ -6,24 +6,28 @@ will be expanded, of course, but for now I'm just getting the feel for
 what's going on.
 """
 
-from psyexp.experiment import Trial
+# from psyexp.experiment import Trial
 import pyglet as pyg
 
 
-# def generate_trials(stimlist, **kwargs):
-#     trial_set = [TextTrial(stimulus=stim, **kwargs) for stim in stimlist]
-#     return trial_set
+def present_text(stimulus, **kwargs):
+    pyg.text.Label(stimulus,
+                   font_name="Courier New",
+                   font_size=36,
+                   anchor_x="center",
+                   anchor_y="center"
+                   ).draw()
 
 
-class TextTrial(Trial):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        # Change the font sometime.
-        self.stimulus = pyg.text.Label(self.stimulus,
-                                       font_name="Courier New",
-                                       font_size=36,
-                                       anchor_x="center", anchor_y="center")
-        self.duration = kwargs.get("duration", 10000)/1000.0
-
-    def go(self):
-        self.stimulus.draw()
+# class TextTrial(Trial):
+#     def __init__(self, **kwargs):
+#         super().__init__(**kwargs)
+#         # Change the font sometime.
+#         self.stimulus = pyg.text.Label(self.stimulus,
+#                                        font_name="Courier New",
+#                                        font_size=36,
+#                                        anchor_x="center", anchor_y="center")
+#         self.duration = kwargs.get("duration", 10000)/1000.0
+#
+#     def go(self):
+#         self.stimulus.draw()
--- test-text.py
+++ test-text.py
@@ -10,14 +10,14 @@ wordlist = utils.load_list("test-list.csv", sep=",", shuffle=True)
 # sys.exit(0)
 
 e = expt.Experiment()
-
 e.allowed_keys = ["Z", "/"]
-ldt_trials = text.generate_trials(wordlist, jitter_mean=400, jitter_sd=50)
-ldt_task = expt.generate_task(ldt_trials)
+
+params = {"jitter": True, "jitter_mean": 400, "jitter_sd": 50}
+
+# ldt_trials = text.generate_trials(wordlist, jitter_mean=400, jitter_sd=50)
+# ldt_task = expt.generate_task(ldt_trials)
+ldt_task = expt.generate_task(wordlist, text.present_text, **params)
 ldt_data = expt.run_task(ldt_task)
-# ldt_data = expt.run_task(expt.generate_task(text.generate_trials(wordlist,
-#                                             "Stimulus", jitter=True,
-#                                             jitter_mean=400, jitter_sd=50)))
 
 e.run()
 
